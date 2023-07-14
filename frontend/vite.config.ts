1546796cea95247b59de8e983f661a2be132666d
Keshav Garg <keshav2003garg@gmail.com>
2023-07-15 02:39:50 +0530

    chore- [vite config & clear folder structure]

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
-// https://vitejs.dev/config/
+const srcPath = path.resolve('./src');
+
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@/components': `${srcPath}/components`,
+        },
+    },
+});
